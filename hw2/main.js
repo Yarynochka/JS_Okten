0647671c56bf9326096d5a0a5c10f3bd4a90436f
Yarynochka <jaryna.games@gmail.com>
2022-11-30 11:07:55 +0200

    hw-2

---
+++ hw2/main.js
@@ -0,0 +1,89 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консольco
+// const arr1=[1,-2,'string',false, 0, true];
+// console.log(arr1[0]);
+// console.log(arr1[1]);
+// console.log(arr1[2]);
+// console.log(arr1[3]);
+// console.log(arr1[4]);
+// console.log(arr1[5]);
+// ////
+// console.log('_____using iter_____')
+// for (const arr1Element of arr1) {
+//     console.log(arr1Element);
+// }
+//
+// console.log(arr1);
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+// const books=[
+//     {
+//         title: 'book1',
+//         pageCount: 257,
+//         genre:'fantasy '
+//     },
+//     {
+//         title: 'book2',
+//         pageCount: 546,
+//         genre:'detective'
+//     },
+//     {
+//         title: 'book3',
+//         pageCount: 425,
+//         genre:'novel'
+//     }
+// ]
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+
+// const books=[
+//     {
+//         title: 'book1',
+//         pageCount: 257,
+//         genre:'fantasy ',
+//         authors:['author1','author2']
+//     },
+//     {
+//         title: 'book2',
+//         pageCount: 546,
+//         genre:'detective',
+//         authors:['author3','author4','author5']
+//     },
+//     {
+//         title: 'book3',
+//         pageCount: 425,
+//         genre:'novel',
+//         authors:['author6']
+//     }
+// ]
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач".
+// Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+// let users=[
+//     {
+//         name:'kolya',
+//         username:'kalyaha',
+//         password: 'hello125hk'
+//     },
+//     {
+//         name:'petya',
+//         username:'petryha',
+//         password: 'heyfk85'
+//     },
+//     {
+//         name:'olha',
+//         username:'olala',
+//         password: 'rgjT543'
+//     }
+// ]
+// console.log(users[0].password);
+// console.log(users[1].password);
+// console.log(users[2].password);
+// console.log('----using iter----')
+// for (const user of users) {
+//     console.log(user.password);
+// }
+
+
+
+
